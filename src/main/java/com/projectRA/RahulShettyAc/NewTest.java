f8ea6e9df87a66d74b1c469a249dbec3383b9f87
SavanthR <95523613+SavanthR@users.noreply.github.com>
2025-04-27 22:46:34 +0530

    Add till data table

---
+++ src/main/java/com/projectRA/RahulShettyAc/NewTest.java
@@ -0,0 +1,9 @@
+		package com.projectRA.RahulShettyAc;
+		
+		public class NewTest {
+		
+			public static void main(String[] args) {
+				System.out.println("Code execution started");
+			}
+		
+		}
--- src/main/java/com/projectRA/RahulShettyAc/Test.java
+++ src/main/java/com/projectRA/RahulShettyAc/Test.java
@@ -1,23 +1,20 @@
 package com.projectRA.RahulShettyAc;
 
-
-
-
 import java.time.Duration;
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Test {
 
-	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "C:\\WORK\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");	
+    public static void main(String[] args) throws InterruptedException {
+    	
+    	System.setProperty("webdriver.chrome.driver", "C:\\WORK\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");	
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
@@ -51,15 +48,35 @@ public class Test {
 	              break;
 	          }
 	      }
-		
-		
-		
-		
-		driver.quit();
-}
-}
+//    	 System.out.println("Code execution started");
+//        WebDriverManager.chromedriver().setup(); // Setup first
+//        WebDriver driver = new ChromeDriver();   // Then create driver
 
+        driver.manage().window().maximize();
+      //  WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
+        WebElement table = driver.findElement(By.xpath("//*[@id='product' and @class='table-display']"));
+        List<WebElement> rows = table.findElements(By.tagName("tr"));
 
+        for (int i = 1; i < rows.size(); i++) {  
+            WebElement row = rows.get(i);
+            List<WebElement> columns = row.findElements(By.tagName("th"));
 
+            for (int j = 0; j < columns.size(); j++) {
+                String cellText = columns.get(j).getText();
+                
+                
+                if(cellText.equals("WebSecurity Testing for Beginners-QA knowledge to next level")) {
+                    System.out.println("cell data: " + cellText );
+                }
+                System.out.println(cellText + " | ");
+            }
+            System.out.println();
+        }
+        System.out.println("Code executed");
+        driver.quit();
+    }
+    }
+   
